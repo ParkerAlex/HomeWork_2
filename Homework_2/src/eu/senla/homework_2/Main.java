23b96b600523deab51c19af0508169a40952d1a5
KIBERS <KIBERS@192.168.43.182>
2021-01-21 16:47:09 +0300

    Ready(LATINICA)

--- Homework_2/src/eu/senla/homework_2/Main.java
+++ Homework_2/src/eu/senla/homework_2/Main.java
@@ -12,105 +12,113 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("ÂÂåäèòå ÷èñëî îò 1 äî 7 èëè îò 11 äî 77");
+	//	System.out.println("ÂÂåäèòå ÷èñëî îò 1 äî 7 èëè îò 12 äî 76");
+		System.out.println("Vvedite chislo ot 1 äî 7 ili ot 12 äî 76");
 		int number = input.nextInt();
 		
 		
 		String[] color = rainbow.getColor();
 		String[] twocolor = rainbow.getTwoColor();
 		
-		if (number >=1 && number <=7)
-		  {
+		  int a = number / 10;
+		  int b = number % 10;
 		
-				switch(number) {
-				  case 1:
-					  System.out.println(color[0]);
-				break;
-				  case 2:
-					  System.out.println(color[1]);
-				break;
-				  case 3:
-					  System.out.println(color[2]);
-				break;
-				  case 4:
-					  System.out.println(color[3]);
-				break;
-				  case 5:
-					  System.out.println(color[4]);
-				break;
-				  case 6:
-					  System.out.println(color[5]);
-				break;
-				  case 7:
-					  System.out.println(color[6]);
-				break;
+		    if (a != b) 
+		    {		  
+				if (number >=1 && number <=7)
+				  {
 				
-				}
+						switch(number) {
+						  case 1:
+							  System.out.println(color[0]);
+						break;
+						  case 2:
+							  System.out.println(color[1]);
+						break;
+						  case 3:
+							  System.out.println(color[2]);
+						break;
+						  case 4:
+							  System.out.println(color[3]);
+						break;
+						  case 5:
+							  System.out.println(color[4]);
+						break;
+						  case 6:
+							  System.out.println(color[5]);
+						break;
+						  case 7:
+							  System.out.println(color[6]);
+						break;
+						
+						}
+							
+					}
+				  else	if (number > 11 && number < 77) {
+					  			  
+					  switch(a) {
+					  case 1:
+						  System.out.print(twocolor[0]  + " - ");
+					break;
+					  case 2:
+						  System.out.print(twocolor[1] + " - ");
+					break;
+					  case 3:
+						  System.out.print(twocolor[2] + " - ");
+					break;
+					  case 4:
+						  System.out.print(twocolor[3] + " - ");
+					break;
+					  case 5:
+						  System.out.print(twocolor[4] + " - ");
+					break;
+					  case 6:
+						  System.out.print(twocolor[5] + " - ");
+					break;
+					  case 7:
+						  System.out.print(twocolor[6] + " - ");
+					break;
 					
-			}
-		  else	if (number > 8 && number < 78) {
-			  
-			  int a = number / 10;
-			  int b = number % 10;
-			  System.out.println(a + " è " + b);
-			  
-			  switch(a) {
-			  case 1:
-				  System.out.print(twocolor[0]  + " - ");
-			break;
-			  case 2:
-				  System.out.print(twocolor[1] + " - ");
-			break;
-			  case 3:
-				  System.out.print(twocolor[2] + " - ");
-			break;
-			  case 4:
-				  System.out.print(twocolor[3] + " - ");
-			break;
-			  case 5:
-				  System.out.print(twocolor[4] + " - ");
-			break;
-			  case 6:
-				  System.out.print(twocolor[5] + " - ");
-			break;
-			  case 7:
-				  System.out.print(twocolor[6] + " - ");
-			break;
-			
-			}
-			  
-			  switch(b) {
-			  case 1:
-				  System.out.println(color[0]);
-			break;
-			  case 2:
-				  System.out.println(color[1]);
-			break;
-			  case 3:
-				  System.out.println(color[2]);
-			break;
-			  case 4:
-				  System.out.println(color[3]);
-			break;
-			  case 5:
-				  System.out.println(color[4]);
-			break;
-			  case 6:
-				  System.out.println(color[5]);
-			break;
-			  case 7:
-				  System.out.println(color[6]);
-			break;
-			
-			}
+					}
+					  
+					  switch(b) {
+					  case 1:
+						  System.out.println(color[0]);
+					break;
+					  case 2:
+						  System.out.println(color[1]);
+					break;
+					  case 3:
+						  System.out.println(color[2]);
+					break;
+					  case 4:
+						  System.out.println(color[3]);
+					break;
+					  case 5:
+						  System.out.println(color[4]);
+					break;
+					  case 6:
+						  System.out.println(color[5]);
+					break;
+					  case 7:
+						  System.out.println(color[6]);
+					break;
 					
-				}
-		  else {
-			     System.out.println("ÂÂåäåíî íåïğàâèëüíîå ÷èñëî");
-		       }
-					 
-	    	}
-		
-	}
+					}
+							
+						}
+				  else {
+					     //System.out.println("ÂÂåäåíî íåïğàâèëüíîå ÷èñëî");
+					  System.out.println("Vvedeno nepravilno chislo");
+				       }
+		     }
+			else { 
+				//  System.out.println("ÂÂåäåíî äâîéíîå îäèíàêîâîå ÷èñëî, ïîëóöâåòà áûòü íå ìîæåò");
+				  System.out.println("Vvedeno dvoinoe odinakovoe chislo, polucveta ne bivaet");
+				 }	 
+			}
+	    	
+	    }
+	
 
 
--- Homework_2/src/eu/senla/homework_2/Rainbow.java
+++ Homework_2/src/eu/senla/homework_2/Rainbow.java
@@ -2,8 +2,11 @@ package eu.senla.homework_2;
 
 public class Rainbow {
 	
-	static final String[] color = {"красный", "оранжевый", "жёлтый", "зелёный", "голубой", "синий", "фиолетовый"};
-	static final String[] twoColor = {"красно", "оранжево", "жёлто", "зелёно", "голубо", "сине", "фиолетово"};
+//	static final String[] color = {"красный", "оранжевый", "жёлтый", "зелёный", "голубой", "синий", "фиолетовый"};
+//	static final String[] twoColor = {"красно", "оранжево", "жёлто", "зелёно", "голубо", "сине", "фиолетово"};
+	
+	static final String[] color = {"krasyiy", "oranjeviy", "jeltiy", "zeleniy", "goluboy", "siniy", "fioletoviy"};
+	static final String[] twoColor = {"krasno", "oranjevo", "jelto", "zeleno", "golubo", "sine", "fioletovo"};
 	
 	public static String[] getColor() {
 		return color;
